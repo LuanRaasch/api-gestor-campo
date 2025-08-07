0fd33f42d91768f1fc81b12556f43964d3aed915
LuanRaasch <luanraasch@outlook.com>
2025-08-06 21:03:39 -0300

    o modulo clientes agora esta funcional

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -1,5 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { ClientesService } from './clientes.service';
+import { Cliente } from './entities/cliente.entity';
 
 @Controller('clientes')
 // eslint-disable-next-line prettier/prettier
-export class ClientesController {}
+export class ClientesController {
+    constructor(private readonly clientesService: ClientesService) {}
+
+    @Get()
+    findAll(): Promise<Cliente[]> {
+        return this.clientesService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<Cliente | null> {
+        return this.clientesService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() usuario: Partial<Cliente>): Promise<Cliente> {
+        return this.clientesService.create(usuario);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() usuario: Partial<Cliente>): Promise<Cliente | null> {
+        return this.clientesService.update(id, usuario);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number): Promise<void> {
+        return this.clientesService.remove(id);
+    }
+}
--- src/clientes/clientes.service.ts
+++ src/clientes/clientes.service.ts
@@ -1,5 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cliente } from './entities/cliente.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 // eslint-disable-next-line prettier/prettier
-export class ClientesService {}
+export class ClientesService {
+    constructor(
+        @InjectRepository(Cliente)
+        private readonly clienteRepository: Repository<Cliente>,
+    ) {}
+
+    findAll(): Promise<Cliente[]> {
+        return this.clienteRepository.find();
+    }
+
+    findOne(id: number): Promise<Cliente | null> {
+        return this.clienteRepository.findOne({ where: { id } });
+    }
+
+    create(cliente: Partial<Cliente>): Promise<Cliente> {
+        return this.clienteRepository.save(cliente);
+    }
+
+    async update(id: number, usuario: Partial<Cliente>): Promise<Cliente | null> {
+        await this.clienteRepository.update(id, usuario);
+        return this.findOne(id);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.clienteRepository.delete(id);
+        return undefined;
+    }
+
+}
