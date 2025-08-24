3f65055d08245a7e31021c9a968c255b7ecd0b4a
LuanRaasch <luanraasch@outlook.com>
2025-08-24 19:22:53 -0300

    chore: att politica do cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  // Habilita CORS para o frontend em localhost:3000
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // se precisar enviar cookies
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
